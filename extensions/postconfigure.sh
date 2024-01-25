711063a40c8c9746e0e5d4349bb22e13e14963b8
NAGANO Osamu <onagano@redhat.com>
2024-01-25 17:30:30 +0900

    add keycloak config modified

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "=== Executing postconfigure.sh"
-$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/adapter-elytron-install-offline.cli
+$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/adapter-elytron-install-offline.cli -Dserver.config=standalone-openshift.xml
 $JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/config-database.cli
